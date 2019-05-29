6271c5bec71c4e8d9cba1c2b83e04a56ad2e2d9d
Roy Margasa <roy-aleph@Roys-MacBook-Pro.local>
2019-05-29 19:05:06 +0700

    fixing bin

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-import { exportSpecifier } from "@babel/types";
-
-/* var calculator = require('./calculator');
-
-var a = 10, b = 5;
-
-console.log("Addition : " + calculator.add(a, b));
-console.log("Subtraction : " + calculator.subtract(a, b));
-console.log("Multiplication : " + calculator.multiply(a, b)); */
-
-
-
-const cmd = () => {
-  console.log('test');
-}
-
-module.exports = {
-  cmd: cmd
-}
+#!/usr/bin/env node
+console.log('testing');
